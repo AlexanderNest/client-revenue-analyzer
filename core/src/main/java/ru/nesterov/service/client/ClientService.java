00dfae8edbc24ac0d3c858616f73344be28aa113
Aleksandra Li <li0tvali@yandex.ru>
2024-08-12 09:24:31 +0300

    импорты классов в сервисе

--- core/src/main/java/ru/nesterov/service/client/ClientService.java
+++ core/src/main/java/ru/nesterov/service/client/ClientService.java
@@ -1,7 +1,9 @@
 package ru.nesterov.service.client;
 
 import ru.nesterov.service.dto.ClientDto;
+import ru.nesterov.service.monthHelper.MonthDatesPair;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ClientService {
