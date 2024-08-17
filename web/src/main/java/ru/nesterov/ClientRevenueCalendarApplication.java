01ccf4455b85c427f0f4e95a7f696f9fc6b4bc32
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-08-17 15:20:59 +0700

    refactor

--- web/src/main/java/ru/nesterov/ClientRevenueCalendarApplication.java
+++ web/src/main/java/ru/nesterov/ClientRevenueCalendarApplication.java
@@ -2,9 +2,8 @@ package ru.nesterov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties
+
 @SpringBootApplication
 public class ClientRevenueCalendarApplication {
     public static void main(String[] args) {
