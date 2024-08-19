5a057f68551bc8a1ba2407eb085a5b9c2c07d671
Aleksandra Li <li0tvali@yandex.ru>
2024-08-19 15:12:42 +0300

    написала еще 2 теста

--- web/src/test/java/ru/nesterov/controller/ClientControllerTest.java
+++ web/src/test/java/ru/nesterov/controller/ClientControllerTest.java
@@ -51,4 +51,56 @@ class ClientControllerTest {
                 .andExpect(jsonPath("$.description").value("desc"))
                 .andExpect(jsonPath("$.pricePerHour").value(100));
     }
+
+    @Test
+    void createClientWithTheSameNameWithoutIdGeneration() throws Exception {
+        CreateClientRequest createClientRequest = new CreateClientRequest();
+        createClientRequest.setDescription("desc");
+        createClientRequest.setName("Masha");
+        createClientRequest.setPricePerHour(100);
+        createClientRequest.setIdGenerationNeeded(false);
+        CreateClientRequest createClientRequest2 = new CreateClientRequest();
+        createClientRequest2.setDescription("desc");
+        createClientRequest2.setName("Masha");
+        createClientRequest2.setPricePerHour(1000);
+        createClientRequest2.setIdGenerationNeeded(false);
+        mockMvc.perform(
+                post(CREATE_CLIENT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createClientRequest)))
+                .andExpect(status().isOk());
+        mockMvc.perform(
+                post(CREATE_CLIENT_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createClientRequest2)))
+                .andExpect(status().is5xxServerError());
+    }
+
+    @Test
+    void createClientWithTheSameNameWithIdGeneration() throws Exception {
+        CreateClientRequest createClientRequest = new CreateClientRequest();
+        createClientRequest.setDescription("desc");
+        createClientRequest.setName("Misha");
+        createClientRequest.setPricePerHour(100);
+        createClientRequest.setIdGenerationNeeded(false);
+        CreateClientRequest createClientRequest2 = new CreateClientRequest();
+        createClientRequest2.setDescription("desc");
+        createClientRequest2.setName("Misha");
+        createClientRequest2.setPricePerHour(1000);
+        createClientRequest2.setIdGenerationNeeded(true);
+        mockMvc.perform(
+                        post(CREATE_CLIENT_URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(createClientRequest)))
+                .andExpect(status().isOk());
+        mockMvc.perform(
+                        post(CREATE_CLIENT_URL)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(createClientRequest2)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.name").value("Misha 2"))
+                .andExpect(jsonPath("$.description").value("desc"))
+                .andExpect(jsonPath("$.pricePerHour").value(1000));
+    }
 }
