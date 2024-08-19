798be20b47cc8e8ba0f5879d7c97cbc89fc3bfb6
StanislavShirokikh <shirokikhstanislav@icloud.com>
2024-08-19 12:30:21 +0700

    refactor

--- web/src/main/test/java/ru/nesterov/controller/event/EventsAnalyzerControllerTest.java
+++ web/src/main/test/java/ru/nesterov/controller/event/EventsAnalyzerControllerTest.java
@@ -7,22 +7,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.nesterov.controller.EventsAnalyzerController;
 import ru.nesterov.dto.Event;
 import ru.nesterov.entity.Client;
 import ru.nesterov.repository.ClientRepository;
 import ru.nesterov.service.CalendarService;
-import ru.nesterov.service.event.EventsAnalyzerProperties;
-import ru.nesterov.service.event.EventsAnalyzerService;
-import ru.nesterov.service.event.EventsAnalyzerServiceImpl;
-import ru.nesterov.service.status.EventStatusServiceImpl;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,28 +26,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = {
-        EventsAnalyzerController.class,
-        EventsAnalyzerServiceImpl.class,
-        EventStatusServiceImpl.class,
-        EventsAnalyzerProperties.class,
-})
-@TestPropertySource(properties = {
-        "app.calendar.color.successful=1,2,3",
-        "app.calendar.color.cancelled=4,5",
-        "app.calendar.color.requires.shift=",
-        "app.calendar.color.planned=6",
-        "app.analyzer.unpaid-events.range=365"
-})
 class EventsAnalyzerControllerTest {
-    @Autowired
-    private EventsAnalyzerService eventsAnalyzerService;
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private ClientRepository clientRepository;
     @MockBean
     private CalendarService calendarService;
+
 
     @BeforeEach
     void init() {
@@ -108,16 +89,18 @@ class EventsAnalyzerControllerTest {
 
     @Test
     void getUnpaidEvents() throws Exception {
-        LocalDateTime expectedEventStart1 = LocalDateTime.of(2024, 8, 11, 11, 30);
-        LocalDateTime expectedEventStart2 = LocalDateTime.of(2024, 8, 12, 11, 30);
+        LocalDateTime expectedEventStart1 = LocalDateTime.of(2024, 8, 11, 11, 30, 00);
+        LocalDateTime expectedEventStart2 = LocalDateTime.of(2024, 8, 12, 11, 30, 00);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
         mockMvc.perform(get("/events/analyzer/getUnpaidEvents")
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].summary").value("testName2"))
-                .andExpect(jsonPath("$[0].eventStart").value(expectedEventStart1))
+                .andExpect(jsonPath("$[0].eventStart").value(expectedEventStart1.format(formatter)))
                 .andExpect(jsonPath("$[1].summary").value("testName1"))
-                .andExpect(jsonPath("$[1].eventStart").value(expectedEventStart2));
+                .andExpect(jsonPath("$[1].eventStart").value(expectedEventStart2.format(formatter)));
     }
 }
