4e56cb7355df5ad52f3fca8f1b1ed7bd664accc5
alexander <nesterov1441@gmail.com>
2024-08-24 16:09:19 +0300

    added controller standards tests

--- web/src/test/java/ru/nesterov/controller/ControllerStandardsTest.java
+++ web/src/test/java/ru/nesterov/controller/ControllerStandardsTest.java
@@ -1,5 +1,7 @@
 package ru.nesterov.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.junit.jupiter.api.DisplayName;
@@ -7,10 +9,15 @@ import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 import org.reflections.util.ConfigurationBuilder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -81,5 +88,102 @@ public class ControllerStandardsTest {
             assertTrue(controllerClass.getInterfaces().length > 0,
                     "Class " + controllerClass.getName() + " does not implement any interface");
         }
+    }
+
+    @Test
+    @DisplayName("Проверка, что все эндпоинты контроллеров содержатся в Postman коллекции")
+    public void testAllControllerEndpointsInPostmanCollection() throws IOException {
+        final String POSTMAN_COLLECTION_PATH = System.getProperty("user.dir");
+        Path dir = Paths.get(POSTMAN_COLLECTION_PATH).getParent();
+        Path postmanCollectionPath = Paths.get("postman", "google-calendar-clients-analyzer.postman_collection.json");
+        Path postmanPath = dir.resolve(postmanCollectionPath);
+
+        Set<EndpointInfo> postmanEndpoints = new HashSet<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(new File(postmanPath.toString()));
+        JsonNode itemsNode = rootNode.get("item");
+
+        if (itemsNode.isArray()) {
+            for (JsonNode itemNode : itemsNode) {
+                JsonNode requestNode = itemNode.get("request");
+                String method = requestNode.get("method").asText();
+                JsonNode urlNode = requestNode.get("url");
+
+                StringBuilder stringBuilder = new StringBuilder();
+                ArrayList<String> list = new ArrayList<>();
+                for(JsonNode p : urlNode.get("path")) {
+                    list.add(p.toString().replace("\"", ""));
+                }
+
+                String path = String.join("/", list);
+
+                EndpointInfo endpointInfo = new EndpointInfo();
+                endpointInfo.endpoint = path;
+                endpointInfo.method = method;
+                postmanEndpoints.add(endpointInfo);
+            }
+        }
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .forPackages("ru.nesterov.controller")
+                .addScanners(Scanners.TypesAnnotated, Scanners.MethodsAnnotated));
+
+        Set<Class<?>> controllerInterfaces = reflections.getTypesAnnotatedWith(RequestMapping.class);
+
+        for (Class<?> controllerInterface : controllerInterfaces) {
+            Method[] methods = controllerInterface.getDeclaredMethods();
+            for (Method method : methods) {
+                EndpointInfo endpointInfo = getEndpoint(method, controllerInterface);
+                assertTrue(hasEndpointEndsWith(postmanEndpoints, endpointInfo), "Endpoint " + endpointInfo.endpoint + " with method " + endpointInfo.method + " is not present in Postman collection");
+            }
+        }
+    }
+
+    private boolean hasEndpointEndsWith(Set<EndpointInfo> postmanEndpoints, EndpointInfo endpointInfo) {
+        for (EndpointInfo postmanEndpoint : postmanEndpoints) {
+            if (postmanEndpoint.method.equals(endpointInfo.method) && postmanEndpoint.endpoint.endsWith(endpointInfo.endpoint)) {
+                return true;
+            }
+        }
+
+        return false;
+//        return postmanEndpoints.stream()
+//                .anyMatch(ep -> ep.endsWith(endpoint));
+    }
+
+    private EndpointInfo getEndpoint(Method method, Class<?> controllerInterface) {
+        EndpointInfo endpointInfo = new EndpointInfo();
+
+        if (method.isAnnotationPresent(GetMapping.class)) {
+            GetMapping getMapping = method.getAnnotation(GetMapping.class);
+            endpointInfo.endpoint = getMapping.value().length > 0 ? getMapping.value()[0] : "";
+            endpointInfo.method = "GET";
+        } else if (method.isAnnotationPresent(PostMapping.class)) {
+            PostMapping postMapping = method.getAnnotation(PostMapping.class);
+            endpointInfo.endpoint = postMapping.value().length > 0 ? postMapping.value()[0] : "";
+            endpointInfo.method = "POST";
+        } else if (method.isAnnotationPresent(PutMapping.class)) {
+            PutMapping putMapping = method.getAnnotation(PutMapping.class);
+            endpointInfo.endpoint = putMapping.value().length > 0 ? putMapping.value()[0] : "";
+            endpointInfo.method = "PUT";
+        } else if (method.isAnnotationPresent(DeleteMapping.class)) {
+            DeleteMapping deleteMapping = method.getAnnotation(DeleteMapping.class);
+            endpointInfo.endpoint = deleteMapping.value().length > 0 ? deleteMapping.value()[0] : "";
+            endpointInfo.method = "DELETE";
+        } else if (method.isAnnotationPresent(RequestMapping.class)) {
+            RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+            endpointInfo.endpoint = requestMapping.value().length > 0 ? requestMapping.value()[0] : "";
+            endpointInfo.method = requestMapping.method()[0].name();
+        }
+
+        String prefix = controllerInterface.getAnnotation(RequestMapping.class).value()[0];
+        endpointInfo.endpoint = prefix + endpointInfo.endpoint;
+
+        return endpointInfo;
+    }
+
+    private static class EndpointInfo {
+        String method;
+        String endpoint;
     }
 }
