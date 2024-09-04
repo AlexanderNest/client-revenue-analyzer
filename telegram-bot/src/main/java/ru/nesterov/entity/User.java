b4c416414f34034c26aa6269f376cc1b3ba9f5de
Aleksandra Li <li0tvali@yandex.ru>
2024-09-04 14:07:01 +0300

    создала бд с пользователями бота

---
+++ telegram-bot/src/main/java/ru/nesterov/entity/User.java
@@ -0,0 +1,24 @@
+package ru.nesterov.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table(name = "user")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_gen")
+    @SequenceGenerator(name = "user_seq_gen", sequenceName = "user_seq")
+    private long id;
+    private String username;
+    private boolean active;
+}
