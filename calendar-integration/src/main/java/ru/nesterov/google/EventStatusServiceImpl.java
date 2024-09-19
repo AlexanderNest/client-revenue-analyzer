20ee955a5f9a879b70295fdb1c14d8ffcd2cfbec
Lizogubov Eugeny <noobikrus@gmail.com>
2024-09-19 19:12:20 +0500

    Refactored

--- calendar-integration/src/main/java/ru/nesterov/google/EventStatusServiceImpl.java
+++ calendar-integration/src/main/java/ru/nesterov/google/EventStatusServiceImpl.java
@@ -20,40 +20,34 @@ public class EventStatusServiceImpl implements EventStatusService {
                                   @Value("${app.calendar.color.requires.shift}") List<String> requiresShiftColorCodes,
                                   @Value("${app.calendar.color.planned}") List<String> plannedColorCodes) {
 
-        //TODO требуется рефакторинг
-
         boolean nullWasUsed = false;
         this.plannedColorCodes = plannedColorCodes;
         if (plannedColorCodes.isEmpty()) {
-            if(nullWasUsed) {
-                throw new IllegalArgumentException("Default calendar color as null was used already");
-            }
-            plannedColorCodes.add(null);
+            this.plannedColorCodes.add(null);
             nullWasUsed = true;
         }
         this.cancelledColorCodes = cancelledColorCodes;
         if (cancelledColorCodes.isEmpty()) {
-            if(nullWasUsed) {
+            if (nullWasUsed) {
                 throw new IllegalArgumentException("Default calendar color as null was used already");
             }
-            cancelledColorCodes.add(null);
+            this.cancelledColorCodes.add(null);
             nullWasUsed = true;
         }
         this.requiresShiftColorCodes = requiresShiftColorCodes;
         if (requiresShiftColorCodes.isEmpty()) {
-            if(nullWasUsed) {
+            if (nullWasUsed) {
                 throw new IllegalArgumentException("Default calendar color as null was used already");
             }
-            requiresShiftColorCodes.add(null);
+            this.requiresShiftColorCodes.add(null);
             nullWasUsed = true;
         }
         this.successColorCodes = successColorCodes;
         if (successColorCodes.isEmpty()) {
-            if(nullWasUsed) {
+            if (nullWasUsed) {
                 throw new IllegalArgumentException("Default calendar color as null was used already");
             }
-            successColorCodes.add(null);
-            nullWasUsed = true;
+            this.successColorCodes.add(null);
         }
     }
 
