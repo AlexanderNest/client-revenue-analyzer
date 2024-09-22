624434d998ad4197aa7ea6b812e78526ef9c0ab5
Uenay <59786905+Uenay@users.noreply.github.com>
2024-09-22 11:08:28 +0300

    доработать добавление клиента #51 (#67)

--- core/src/main/java/ru/nesterov/service/dto/ClientDto.java
+++ core/src/main/java/ru/nesterov/service/dto/ClientDto.java
@@ -3,6 +3,8 @@ package ru.nesterov.service.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Builder
 public class ClientDto {
@@ -11,4 +13,6 @@ public class ClientDto {
     private int pricePerHour;
     private String description;
     private boolean active;
+    private Date startDate;
+    private String phone;
 }
