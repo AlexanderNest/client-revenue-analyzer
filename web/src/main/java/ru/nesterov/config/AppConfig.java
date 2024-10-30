482404b2de9bb72d2a2e4a5aa5f6be2b2e80f410
Lizogubov Eugeny <noobikrus@gmail.com>
2024-10-30 17:18:22 +0500

    Revert "123"
    This reverts commit f1d4b0670e9be4954de9ebdafc9c636fdd3596df.

--- web/src/main/java/ru/nesterov/config/AppConfig.java
+++ web/src/main/java/ru/nesterov/config/AppConfig.java
@@ -1,26 +1,9 @@
 package ru.nesterov.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.nesterov.controller.EventsBackupController;
-import ru.nesterov.controller.EventsBackupControllerImpl;
-import ru.nesterov.google.GoogleCalendarService;
-import ru.nesterov.repository.EventBackupRepository;
-import ru.nesterov.repository.UserRepository;
-import ru.nesterov.service.event.EventBackupProperties;
-import ru.nesterov.service.event.EventsBackupService;
 
 @Configuration
 @EnableScheduling
 public class AppConfig {
-    @Bean
-    @ConditionalOnProperty(name = "backup.enable")
-    public EventsBackupService eventsBackupService(UserRepository userRepository,
-                                                   GoogleCalendarService googleCalendarService,
-                                                   EventBackupProperties eventBackupProperties,
-                                                   EventBackupRepository eventBackupRepository) {
-        return new EventsBackupService(userRepository, googleCalendarService, eventBackupProperties, eventBackupRepository);
-    }
 }
