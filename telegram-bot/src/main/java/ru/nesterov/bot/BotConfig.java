1919f5507506f27b7af027c8e460a74c7dd9cc5e
alexander <nesterov1441@gmail.com>
2024-11-01 16:23:49 +0300

    refactoring

--- telegram-bot/src/main/java/ru/nesterov/bot/BotConfig.java
+++ telegram-bot/src/main/java/ru/nesterov/bot/BotConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -17,5 +18,10 @@ public class BotConfig {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         telegramBotsApi.registerBot(revenueAnalyzerBot);
         return telegramBotsApi;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
