d20d0ce70f5ed14385e986c5ad508abb0e5179a4
uenay <uenay1@gmail.com>
2024-12-01 10:34:46 +0300

    add UserSettings

--- core/src/main/java/ru/nesterov/repository/UserSettingsRepository.java
+++ core/src/main/java/ru/nesterov/repository/UserSettingsRepository.java
@@ -1,5 +1,13 @@
 package ru.nesterov.repository;
 
-public interface UserSettingsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.nesterov.entity.User;
+import ru.nesterov.entity.UserSetting;
 
+import java.util.List;
+
+@Repository
+public interface UserSettingsRepository extends JpaRepository<UserSetting, Long> {
+    List<User> findAllBy(boolean isEventsBackupEnabled);
 }
