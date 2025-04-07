5e09156585f27cb4356be99b0245169ec915218f
Aoexe <nikitatolmach@gmail.com>
2025-04-07 16:01:10 +0300

    07.04

--- telegram-bot/src/test/java/ru/nesterov/bot/handlers/RegisteredUserHandlerTest.java
+++ telegram-bot/src/test/java/ru/nesterov/bot/handlers/RegisteredUserHandlerTest.java
@@ -1,8 +1,12 @@
 package ru.nesterov.bot.handlers;
 
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
+import ru.nesterov.bot.handlers.implementation.CreateClientHandler;
+import ru.nesterov.bot.handlers.implementation.CreateUserHandler;
 import ru.nesterov.bot.handlers.implementation.UpdateUserControlButtonsHandler;
 import ru.nesterov.bot.handlers.service.ButtonCallbackService;
 
@@ -10,9 +14,11 @@ import ru.nesterov.bot.handlers.service.ButtonCallbackService;
  * Базовый тест для Handler для работы с зарегистрированным пользователем. Содержит основные бины, которые используют обработчики.
  */
 @ContextConfiguration(classes = {
-        ButtonCallbackService.class
+        ButtonCallbackService.class,
+        UpdateUserControlButtonsHandler.class,
+        CreateUserHandler.class
 })
 public abstract class RegisteredUserHandlerTest extends AbstractHandlerTest {
-    @MockBean
-    protected UpdateUserControlButtonsHandler unregisteredUserHandler;
+    @Autowired
+    protected UpdateUserControlButtonsHandler updateUserControlButtonsHandler;
 }
--- telegram-bot/src/test/java/ru/nesterov/bot/handlers/implementation/UpdateUserControlButtonsHandlerTestTest.java
+++ telegram-bot/src/test/java/ru/nesterov/bot/handlers/implementation/UpdateUserControlButtonsHandlerTestTest.java
@@ -10,6 +10,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import ru.nesterov.bot.TelegramUpdateUtils;
 import ru.nesterov.bot.handlers.RegisteredUserHandlerTest;
@@ -19,12 +20,12 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 
 @ContextConfiguration(classes = {
         GetClientScheduleCommandHandler.class,
         GetMonthStatisticsCommandHandler.class,
-        UpdateUserControlButtonsHandler.class,
         BotProperties.class
 })
 @EnableConfigurationProperties(BotProperties.class)
@@ -33,19 +34,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 })
 public class UpdateUserControlButtonsHandlerTestTest extends RegisteredUserHandlerTest {
     @Autowired
-    private UpdateUserControlButtonsHandler updateUserControlButtonsHandler;
-    @Autowired
     private BotProperties botProperties;
 
     @Test
-    public void test() {
+    public void testForRegisteredUser() {
         Update update = new Update();
         Message message = new Message();
         Chat chat = new Chat();
         chat.setId(111L);
+        User user = new User();
+        user.setId(1L);
+        message.setFrom(user);
         message.setChat(chat);
         message.setText("/start");
         update.setMessage(message);
+        when(client.getUserByUsername(/*request*/null)).thenReturn(null);
 
         BotApiMethod<?> result = updateUserControlButtonsHandler.handle(update);
 
@@ -63,5 +66,10 @@ public class UpdateUserControlButtonsHandlerTestTest extends RegisteredUserHandl
         replyKeyboardMarkup.getKeyboard().stream()
                 .map(ArrayList::size)
                 .forEach(size -> assertEquals(1, size));
+    }
+
+    @Test
+    public void testForUnregisteredUser(){
+
     }
 }
