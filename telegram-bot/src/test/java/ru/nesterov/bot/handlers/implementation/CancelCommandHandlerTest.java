30b2391ccc2bfee95c87f1d7c331a19e917bda52
Aoexe <nikitatolmach@gmail.com>
2025-05-04 00:27:21 +0300

    test

--- telegram-bot/src/test/java/ru/nesterov/bot/handlers/implementation/CancelCommandHandlerTest.java
+++ telegram-bot/src/test/java/ru/nesterov/bot/handlers/implementation/CancelCommandHandlerTest.java
@@ -1,4 +1,20 @@
 package ru.nesterov.bot.handlers.implementation;
 
-public class CancelCommandHandlerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import ru.nesterov.bot.handlers.AbstractHandlerTest;
+import ru.nesterov.bot.handlers.service.BotHandlersRequestsKeeper;
+
+@ContextConfiguration(classes = {
+        CancelCommandHandler.class,
+})
+public class CancelCommandHandlerTest  extends AbstractHandlerTest {
+    @Autowired
+    private BotHandlersRequestsKeeper botHandlersRequestsKeeper;
+    @Test
+    void cancelHandlers() {
+
+    }
+
 }
