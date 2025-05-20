184102f122e8dbb322c410c4f57f5c969a058a63
uliana2903 <ulyana.belova.29@yandex.ru>
2025-05-20 17:51:58 +0300

    FIX

---
+++ telegram-bot/src/main/java/ru/nesterov/bot/handlers/implementation/stateful/getSchedule/State.java
@@ -0,0 +1,9 @@
+package ru.nesterov.bot.handlers.implementation.stateful.getSchedule;
+
+public enum State {
+    STARTED,
+    CLIENT_SELECTED,
+    FIRST_DATE_SELECTED,
+    SECOND_DATE_SELECTED,
+    FINISH
+}
