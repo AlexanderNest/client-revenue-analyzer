2e245cc8adb39be87cc4e80ca3ecbcb270b38ac7
Aoexe <nikitatolmach@gmail.com>
2025-06-08 10:55:04 +0300

    add

--- core/src/main/java/ru/nesterov/service/dto/FullClientInfoDto.java
+++ core/src/main/java/ru/nesterov/service/dto/FullClientInfoDto.java
@@ -12,9 +12,12 @@ public class FullClientInfoDto {
     private String name;
     private int pricePerHour;
     private String description;
-    private Date startDate; // ?
+    private Date startDate;
+    private int serviceDuration;
     private String phone;
     private int totalMeetings;
+    private int totalMeetingsHours;
     private int totalIncome;
-    private int cancelledEventsCount;
+    private int unplannedCancelledEventsCount;
+    private int plannedCancelledEventsCount;
 }
