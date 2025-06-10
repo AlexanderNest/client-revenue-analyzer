801d3e608ccee351599a81b594a3d1e1c6cd9f9b
uliana2903 <ulyana.belova.29@yandex.ru>
2025-06-10 10:41:21 +0300

    убрала некоторые аннотации

--- telegram-bot/src/main/java/ru/nesterov/bot/handlers/implementation/GetUnpaidEventsHandler.java
+++ telegram-bot/src/main/java/ru/nesterov/bot/handlers/implementation/GetUnpaidEventsHandler.java
@@ -6,7 +6,6 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.nesterov.bot.dto.GetUnpaidEventsResponse;
 import ru.nesterov.bot.handlers.abstractions.DisplayedCommandHandler;
-import ru.nesterov.bot.handlers.abstractions.Priority;
 import ru.nesterov.bot.utils.TelegramUpdateUtils;
 
 import java.time.format.DateTimeFormatter;
@@ -19,16 +18,6 @@ public class GetUnpaidEventsHandler extends DisplayedCommandHandler {
     @Override
     public String getCommand() {
         return "Узнать неоплаченные события";
-    }
-
-    @Override
-    public Priority getPriority() {
-        return Priority.HIGHEST;
-    }
-
-    @Override
-    public boolean isDisplayedForRegistered() {
-        return super.isDisplayedForRegistered();
     }
 
     @Override
