7599f6278f292873382a314fa81bd090a5069684
uenay <uenay1@gmail.com>
2025-06-22 09:41:33 +0300

    fix error handling

--- core/src/main/java/ru/nesterov/service/client/DataIntegrityViolationExceptionHandler.java
+++ core/src/main/java/ru/nesterov/service/client/DataIntegrityViolationExceptionHandler.java
@@ -11,6 +11,10 @@ import java.util.regex.Pattern;
 @Slf4j
 
 public class DataIntegrityViolationExceptionHandler {
+    private static final Map<String, String> INDEXES = Map.of(
+            "IDX_UNIQUE_PHONE_PER_USER", "Номер телефона",
+            "IDX_UNIQUE_CLIENT_NAME_PER_USER", "Имя клиента"
+    );
     private static String getIndexName(DataIntegrityViolationException dataIntegrityViolationException) {
         Pattern pattern = Pattern.compile("\"(?:PUBLIC\\.)?([A-Za-z0-9_]+)(?:\\s+ON)?");
         Matcher matcher = pattern.matcher(dataIntegrityViolationException.getMessage());
@@ -20,13 +24,8 @@ public class DataIntegrityViolationExceptionHandler {
         return null;
     }
 
-    private static final Map<String, String> indexes = Map.of(
-            "IDX_UNIQUE_PHONE_PER_USER", "Номер телефона",
-            "IDX_UNIQUE_CLIENT_NAME_PER_USER", "Имя клиента"
-    );
-
     public static String getLocalizedMessage(DataIntegrityViolationException exception) {
-        return indexes.get(getIndexName(exception));
+        return INDEXES.get(getIndexName(exception));
     }
 
 }
