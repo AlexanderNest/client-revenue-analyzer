640a1ec70cae1da1819988464a3b6ccd6729c604
uenay <uenay1@gmail.com>
2025-07-06 02:48:23 +0300

    added AsyncUpdateHandler

--- web/src/main/java/ru/nesterov/ClientRevenueCalendarApplication.java
+++ web/src/main/java/ru/nesterov/ClientRevenueCalendarApplication.java
@@ -2,8 +2,9 @@ package ru.nesterov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-
+@EnableAsync
 @SpringBootApplication
 public class ClientRevenueCalendarApplication {
     public static void main(String[] args) {
