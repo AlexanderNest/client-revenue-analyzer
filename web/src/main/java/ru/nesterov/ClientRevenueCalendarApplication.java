8f8d91fd10fb5a74239d09868f8e458debd05f98
alexander <nesterov1441@gmail.com>
2025-07-09 20:31:05 +0300

    some fixes

--- web/src/main/java/ru/nesterov/ClientRevenueCalendarApplication.java
+++ web/src/main/java/ru/nesterov/ClientRevenueCalendarApplication.java
@@ -2,8 +2,13 @@ package ru.nesterov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-
+@EnableWebSecurity
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class ClientRevenueCalendarApplication {
     public static void main(String[] args) {
