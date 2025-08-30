f05e1c23e617fca0fe7c1e787232cf7e21250336
Vitaly-Pon <vitas.ponomaryov1604232@gmail.com>
2025-08-30 21:16:45 +0300

    Доработать client/getSchedule #33 (#251)
    Co-authored-by: alexander <nesterov1441@gmail.com>

--- web/src/main/java/ru/nesterov/web/controller/response/EventScheduleResponse.java
+++ web/src/main/java/ru/nesterov/web/controller/response/EventScheduleResponse.java
@@ -10,4 +10,5 @@ import java.time.LocalDateTime;
 public class EventScheduleResponse {
     private final LocalDateTime eventStart;
     private final LocalDateTime eventEnd;
+    private final boolean requiresShift;
 }
