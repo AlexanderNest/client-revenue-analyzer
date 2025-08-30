9c39c4d4b584dcd340fadf7e03d7261b06062380
alexander <nesterov1441@gmail.com>
2025-08-30 17:14:32 +0300

    немного фиксов

--- telegram-bot/src/main/java/ru/nesterov/bot/dto/CreateUserRequest.java
+++ telegram-bot/src/main/java/ru/nesterov/bot/dto/CreateUserRequest.java
@@ -13,6 +13,6 @@ public class CreateUserRequest {
     private String mainCalendarId;
     private String cancelledCalendarId;
     private String userIdentifier;
-    private Boolean isCancelledCalendarEnabled;
+    private boolean isCancelledCalendarEnabled;
     private String source;
 }
